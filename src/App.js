2e4f630b322e6e86dca764fa57cfb1dbb34dbc2c
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2023-08-01 11:42:02 +0200

    Adding heroes to a file

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import {useState} from "react";
 import CharacterCard from "./components/CharacterCard/CharacterCard";
 import SearchBar from "./components/SearchBar/SearchBar";
 import {Card, Grid, Image} from "semantic-ui-react";
-import blackWidow from "./img/black_widow.jpg";
-import captainAmerica from "./img/captain_america.jpg";
-import hulk from "./img/hulk.jpg";
-import ironman from "./img/ironman.jpg";
-import mystique from "./img/mystique.jpg";
-import scarlet from "./img/scarlet.jpg";
+import featuredHeroes from "./constants/featuredHeroes";
 
 
 
@@ -22,46 +17,6 @@ function App() {
   const timestamp = '1675961395';
   const apiKey = '824b4f2bb67a85e638a19888121bd05a';
   const hashKey = 'd32f224fb3ad8c01de9db005300f1ef6';
-
-  const heroesList = [
-    {
-      id: 1,
-      name: "Scarlet Witch",
-      image: scarlet,
-      description: "Scarlet Witch harnessed probability manipulation for personal gain, then mastered reality-altering magic through training."
-    },
-    {
-      id: 2,
-      name: "Black Widow",
-      image: blackWidow,
-      description: "Natasha Romanoff, the Black Widow, started as an evil agent but now fights for good with stealth and precision."
-    },
-    {
-      id: 3,
-      name: "Mystique",
-      image: mystique,
-      description: "Mystique is a mutant shapeshifter with blue skin, red hair, and yellow eyes, able to mimic anyone with precision."
-    },
-    {
-      id: 4,
-      name: "Ironman",
-      image: ironman,
-      description: "Wounded and captured, Tony Stark builds a life-saving suit of armor, transforming into Iron Man to protect the world."
-    },
-    {
-      id: 5,
-      name: "Captain America",
-      image: captainAmerica,
-      description: "Steve Rogers becomes Captain America after taking a super soldier serum, defending freedom for over 60 years."
-    },
-    {
-      id: 6,
-      name: "Hulk",
-      image: hulk,
-      description: "Dr. Bruce Banner transforms into the Hulk after a gamma bomb explosion, gaining strength with anger."
-    },
-
-  ]
 
 
   const fetchCharacter = async (searchValue) => {
@@ -101,7 +56,7 @@ function App() {
               <div className="featured-heroes">
                 <h2 className="ui header ">Featured Heroes</h2>
                 <Grid stackable columns={3}>
-                  {heroesList.map((character, index) => (
+                  {featuredHeroes.map((character, index) => (
                       <Grid.Column key={character.id}>
                         <Card className="centered">
                           <Image src={character.image} alt={"Thumbnail for " + character.name} />
@@ -134,8 +89,8 @@ function App() {
             </div>
           </div>
           {/*Section for mobile*/}
-          <div className="ui stackable grid mobile only"> {/* Add "stackable" class to make columns stack on mobile view */}
-            <div className="sixteen wide column"> {/* Make the search box take the full width on mobile view */}
+          <div className="ui stackable grid mobile only">
+            <div className="sixteen wide column">
               <div className="ui container searchBox">
                 <h2 className="ui header ">Search</h2>
                 <SearchBar
---
+++ src/constants/featuredHeroes.js
@@ -0,0 +1,48 @@
+import scarlet from "../img/scarlet.jpg";
+import blackWidow from "../img/black_widow.jpg";
+import mystique from "../img/mystique.jpg";
+import ironman from "../img/ironman.jpg";
+import captainAmerica from "../img/captain_america.jpg";
+import hulk from "../img/hulk.jpg";
+
+const featuredHeroes = [
+    {
+        id: 1,
+        name: "Scarlet Witch",
+        image: scarlet,
+        description: "Scarlet Witch harnessed probability manipulation for personal gain, then mastered reality-altering magic through training."
+    },
+    {
+        id: 2,
+        name: "Black Widow",
+        image: blackWidow,
+        description: "Natasha Romanoff, the Black Widow, started as an evil agent but now fights for good with stealth and precision."
+    },
+    {
+        id: 3,
+        name: "Mystique",
+        image: mystique,
+        description: "Mystique is a mutant shapeshifter with blue skin, red hair, and yellow eyes, able to mimic anyone with precision."
+    },
+    {
+        id: 4,
+        name: "Ironman",
+        image: ironman,
+        description: "Wounded and captured, Tony Stark builds a life-saving suit of armor, transforming into Iron Man to protect the world."
+    },
+    {
+        id: 5,
+        name: "Captain America",
+        image: captainAmerica,
+        description: "Steve Rogers becomes Captain America after taking a super soldier serum, defending freedom for over 60 years."
+    },
+    {
+        id: 6,
+        name: "Hulk",
+        image: hulk,
+        description: "Dr. Bruce Banner transforms into the Hulk after a gamma bomb explosion, gaining strength with anger."
+    },
+
+]
+
+export default featuredHeroes;
